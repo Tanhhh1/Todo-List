5042768f770cc701728d2c2fe5fa51acd1715250
Tanhhh1 <anhtranduy25@gmail.com>
2025-10-17 23:07:24 +0700

    fix notification

--- Todo-List/Todo-List/wwwroot/js/notification.js
+++ Todo-List/Todo-List/wwwroot/js/notification.js
@@ -1,8 +1,8 @@
-﻿const alertDiv = document.getElementById("alertMessage");
+﻿const alertDiv = document.getElementById("alertOverlay");
 if (alertDiv) {
     setTimeout(() => {
         alertDiv.style.transition = "opacity 0.5s";
         alertDiv.style.opacity = 0;
         setTimeout(() => alertDiv.remove(), 500); 
-    }, 5000);
+    }, 2500);
 }
